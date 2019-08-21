27ca5e94288a4876803fd57f4526b029c7c494db
see <see@example.com>
2019-08-21 15:50:41 +0800

    init commit

---
+++ src/token.cpp
@@ -0,0 +1,5 @@
+#include <token.hpp>
+#include <secp256k1.h>
+ACTION token::hi( name nm ) {
+	secp256k1_context *ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
+}
